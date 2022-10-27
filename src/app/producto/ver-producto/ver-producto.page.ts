8d8236c6e3cf68cbf3631e5fd2e106b3ca0614c1
Kunisugi <jav.navarrete@duocuc.cl>
2022-10-27 01:32:05 -0300

    comentarios

--- src/app/producto/ver-producto/ver-producto.page.ts
+++ src/app/producto/ver-producto/ver-producto.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { SvProductoService } from './../services/sv-producto.service';
-import { IProducto} from './../modelo/i-producto';
+import { SvProductoService } from './../services/sv-producto.service';//Llamamos al servicios
+import { IProducto} from './../modelo/i-producto'; //Llamamos a la Interfaz de producto
 
 @Component({
   selector: 'app-ver-producto',
@@ -15,12 +15,11 @@ export class VerProductoPage implements OnInit {
   constructor(private activedRoute : ActivatedRoute, private api: SvProductoService) { }
 
   ngOnInit() {
-    this.id = this.activedRoute.snapshot.queryParams['id'];
+    this.id = this.activedRoute.snapshot.queryParams['id']; //Recibimos la id
     console.log(this.id)
 
-    this.api.getProducto(this.id).subscribe(data => {
-      this.producto = data;
-      console.log(this.producto.nombre)
+    this.api.getProducto(this.id).subscribe(data => { //Hacemos un get con la id
+      this.producto = data; //le damos el valor a producto con el producto rescatado
     })
   }
 
